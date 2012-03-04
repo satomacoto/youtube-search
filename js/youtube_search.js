abf3030eec7e63f4ce21df69d2e9dac7a8f39a32
Makoto Sato <sato@sato-air-3.local>
2012-03-05 01:58:43 +0900

    change index.html

--- js/youtube_search.js
+++ js/youtube_search.js
@@ -26,26 +26,22 @@ $().ready(function(){
             if(url.match(/youtu\.be\/([-\w]+)/) || url.match(/v=([-\w]+)/)){
               var id = RegExp.$1;
 
-              // id がすでに存在していたら飛ばす
+              // skip id if exists 
               if (id in ids) return false;
               ids[id] = true;
               
               // http://img.youtube.com/vi/<insert-youtube-video-id-here>/0.jpg 
               var div = $('<div/>')
                 .attr('class', 'thumbnail')
-                .css('width', '80px')
-                .css('height', '80px')
-                .css('float', 'left')
-                .css('background-image', 'url("http://img.youtube.com/vi/' + id + '/1.jpg")')
-                .css('background-position', 'center center')
+                .css({'width': '80px', 'height': '80px', 'float': 'left'})
+                .css({'background-image': 'url("http://img.youtube.com/vi/' + id + '/1.jpg")', 'background-position': 'center center'})
                 .click(function () {
                   $('#player').remove();
                   var player = $('<iframe/>')
                     .attr('id', 'player')
                     .attr('class', 'youtube-player')
                     .attr('type', 'text/html')
-                    .attr('width', '320')
-                    .attr('height', '240')
+                    .attr({'width': '320', 'height': '240'})
                     .attr('src', 'http://www.youtube.com/embed/' + id + '?autoplay=1')
                     .attr('frameborder', '0')
                     .css('float', 'left');
@@ -53,27 +49,16 @@ $().ready(function(){
                   $('#q').select();
                 });
               $('#main').prepend(div);
-              /*
-              var a = $('<a/>').attr('href','http://youtu.be/' + id);
-              a.attr('target','_blank');
-              var img = $('<img/>').attr('src','http://img.youtube.com/vi/' + id + '/1.jpg');
-              img.bind('load',function(){
-                a.hide();
-                a.append(img);
-                $('#main').prepend(a);
-                a.fadeIn();
-              });
-              var div = $('<div/>').attr('id', id);
-              // Lets Flash from another domain call JavaScript
-              var params = { allowScriptAccess: "always" };
-              // The element id of the Flash embed
-              var atts = { id: id };
-              $('#main').append(div);
-              swfobject.embedSWF("http://www.youtube.com/v/" + id + "?version=3&enablejsapi=1&playerapiid=player1", id, "208", "130", "9", null, null, params, atts);
-              */
             }
           }
         });
+        var query_text = $('<div/>')
+          .attr('class', 'thumbnail')
+          .css({'width': '80px', 'height': '80px', 'float': 'left'})
+          .css({'font-size': '40px', 'line-height': '40px', 'overflow': 'hidden'})
+          //.css({'border-style': 'solid', 'border-width': '5px'})
+          .html(query);
+        $('#main').prepend(query_text);
       }
     });
     return false;
